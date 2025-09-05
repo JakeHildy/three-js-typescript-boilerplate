44446b7a01786dceb0f003d88d0b96c875586836
Jake Hildebrandt <jake@zebel.io>
2025-09-04 19:45:46 -0700

    part-way

--- src/main.ts
+++ src/main.ts
@@ -2,25 +2,25 @@ import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
-//import { RGBELoader } from 'three/addons/loaders/RGBELoader.js'
+import { RGBELoader } from "three/addons/loaders/RGBELoader.js";
 import Stats from "three/addons/libs/stats.module.js";
 //import { Lensflare, LensflareElement } from 'three/addons/objects/Lensflare.js'
 
 const scene = new THREE.Scene();
 
-// const light = new THREE.SpotLight(undefined, Math.PI * 1000)
-// light.position.set(5, 5, 5)
-// light.angle = Math.PI / 16
-// light.castShadow = true
-// scene.add(light)
+const light = new THREE.SpotLight(undefined, Math.PI * 1000);
+light.position.set(5, 5, 5);
+light.angle = Math.PI / 16;
+light.castShadow = true;
+scene.add(light);
 
 // const helper = new THREE.SpotLightHelper(light)
 // scene.add(helper)
 
-// new RGBELoader().load('img/venice_sunset_1k.hdr', (texture) => {
-//   texture.mapping = THREE.EquirectangularReflectionMapping
-//   scene.environment = texture
-// })
+new RGBELoader().load("img/venice_sunset_1k.hdr", (texture) => {
+  texture.mapping = THREE.EquirectangularReflectionMapping;
+  scene.environment = texture;
+});
 
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -31,8 +31,8 @@ const camera = new THREE.PerspectiveCamera(
 camera.position.set(1.5, 0.75, 2);
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
-//renderer.toneMapping = THREE.ACESFilmicToneMapping
-//renderer.toneMappingExposure = 0.1
+// renderer.toneMapping = THREE.ACESFilmicToneMapping;
+// renderer.toneMappingExposure = 0.1;
 renderer.shadowMap.enabled = true;
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
@@ -56,11 +56,11 @@ controls.enableDamping = true;
 new GLTFLoader().load("models/suzanne_scene.glb", (gltf) => {
   console.log(gltf);
 
-  // const suzanne = gltf.scene.getObjectByName('Suzanne') as THREE.Mesh
-  // suzanne.castShadow = true
+  const suzanne = gltf.scene.getObjectByName("Suzanne") as THREE.Mesh;
+  suzanne.castShadow = true;
 
-  // const plane = gltf.scene.getObjectByName('Plane') as THREE.Mesh
-  // plane.receiveShadow = true
+  const plane = gltf.scene.getObjectByName("Plane") as THREE.Mesh;
+  plane.receiveShadow = true;
 
   scene.add(gltf.scene);
 });
